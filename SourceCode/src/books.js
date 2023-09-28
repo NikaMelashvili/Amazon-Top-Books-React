052e4dd518f3abcde7322b20b66131bceca48fca
NikaMelashvili <109832550+NikaMelashvili@users.noreply.github.com>
2023-09-28 13:35:16 +0400

    Source Code
    Doesn't contain node_moduls folders due to the size

---
+++ SourceCode/src/books.js
@@ -0,0 +1,38 @@
+export const books = [
+    {
+        title: "The Rabbit Hutch",
+        author: "Tess Gunty",
+        img: "https://images-na.ssl-images-amazon.com/images/I/71Wa3S5DIRL._AC_UL600_SR600,400_.jpg",
+        id: 1
+    },
+    {
+        title: "Fourth Wing (The Empyrean, 1)",
+        author: "Rebecca Yarros",
+        img: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+        id: 2
+    },
+    {
+        title: "Iron Flame (The Empyrean, 2)",
+        author: "Rebecca Yarros",
+        img: "https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL600_SR600,400_.jpg",
+        id: 3
+    },
+    {
+        title: "American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER",
+        author: "Kai Bird, Martin J. Sherwin",
+        img: "https://images-na.ssl-images-amazon.com/images/I/81yfsIOijJL._AC_UL600_SR600,400_.jpg",
+        id: 4
+    },
+    {
+        title: "Outlive: The Science and Art of Longevity",
+        author: "Peter Attia MD, Bill Gifford",
+        img: "https://images-na.ssl-images-amazon.com/images/I/71Pyj+9IPdL._AC_UL600_SR600,400_.jpg",
+        id: 5
+    },
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+        id: 6
+    }
+];
